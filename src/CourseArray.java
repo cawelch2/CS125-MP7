047c8ba6a9b58618fc73609244442afef9f432b4
Philip <Philip@wirelessprv-10-194-186-14.near.illinois.edu>
2017-12-09 22:13:57 -0600

    Added ART courses, test if Git is working.

--- src/CourseArray.java
+++ src/CourseArray.java
@@ -1,13 +1,17 @@
 public class CourseArray {
 	public static Course[] courses = {
-			new Course("CS", 101, "Spring 2018"), 
-			new Course("CS", 105, "Spring 2018"), 
-			new Course("CS", 125, "Spring 2018"), 
-			new Course("CHEM", 101, "Spring 2018"), 
-			new Course("CHEM", 102, "Spring 2018"), 
-			new Course("CHEM", 103, "Spring 2018"), 
-			new Course("MATH", 221, "Spring 2018"), 
-			new Course("MATH", 231, "Spring 2018"), 
-			new Course("MATH", 241, "Spring 2018")
+			new Course("ART", 100, "Understanding Visual Culture"), 
+			new Course("ART", 102, "Drawing for Non-Majors"), 
+			new Course("ART", 103, "Painting for Non-Majors"), 
+			new Course("ART", 104, "Sculpture for Non-Majors"), 
+			new Course("ART", 105, "Visual Design for Non-Majors"), 
+			new Course("ART", 140, "Introduction to ART"), 
+			new Course("ART", 150, "Introduction to Drawing"), 
+			new Course("ART", 151, "Black & White Film Photography"), 
+			new Course("ART", 153, "Digital Photography Seminar"), 
+			new Course("ART", 154, "Digital Video"), 
+			new Course("ART", 155, "Introduction to Graphic Design - Unit One"), 
+			new Course("ART", 199, "Undergraduate Open Seminar"), 
+			new Course("ART", 201, "in Early Childhood")
 		};
 }
